82bd461d3fa5a242e9559c29c04a6c442e1ccb8f
Mike <ori7@sera.net>
2025-05-08 10:40:46 +0900

    updated

--- run-server.sh
+++ run-server.sh
@@ -1,1 +1,3 @@
-sudo caddy_linux_amd64 file-server --listen "$1:$2" --root . --domain "$1"
+# caddy_linux_amd64 file-server --listen "$1:$2" --root . --domain "$1"
+
+python ./https_server.py -c config.toml
